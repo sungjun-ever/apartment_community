89741a66e1139412a76f7bb30e004e0048223508
sungjun-ever <zkfmak10@gmail.com>
2026-04-03 16:17:23 +0900

    main.go 수정 서버 시작 및 종료 코드 추가

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,13 @@ import (
 	"apart_community/internal/database"
 	"apart_community/registry"
 	"apart_community/router"
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -24,4 +31,30 @@ func main() {
 
 	r := gin.Default()
 	router.SetupRouter(r, container)
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Println("서버 종료 중")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("서버 종료: ", err)
+	}
+
+	log.Println("서버 종료 완료")
 }
