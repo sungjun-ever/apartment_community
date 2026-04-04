a3e801f42fb3e78ca564d3b4fcbb75642f58fdc3
sungjun-ever <zkfmak10@gmail.com>
2026-04-04 14:26:12 +0900

    의존성 주입 및 유틸 함수 생성, 적용

--- internal/dto/user/request.go
+++ internal/dto/user/request.go
@@ -1,5 +1,20 @@
 package user
 
+import "apart_community/internal/model"
+
 type UriRequest struct {
 	ID uint `uri:"id" binding:"required,numeric,gt=0"`
+}
+
+type RegisterRequest struct {
+	Email           string `json:"email" binding:"required,email"`
+	Password        string `json:"password" binding:"required,password_check"`
+	PasswordConfirm string `json:"password_confirm" binding:"required,eqfield=Password"`
+}
+
+func (r *RegisterRequest) ToEntity() model.User {
+	return model.User{
+		Email:    r.Email,
+		Password: r.Password,
+	}
 }
