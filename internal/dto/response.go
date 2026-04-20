d38d961c685bf2ca8e03df87171f9cf7eb1ca266
sungjun-ever <zkfmak10@gmail.com>
2026-04-20 13:14:24 +0900

    공통 응답 구조체 변경 성공 시에 빈데이터 응답 가능하도록 data 포인터로 변경

--- internal/dto/response.go
+++ internal/dto/response.go
@@ -3,14 +3,22 @@ package dto
 type Response[T any] struct {
 	Success bool   `json:"success"`
 	Message string `json:"message"`
-	Data    T      `json:"data,omitempty"`
+	Data    *T     `json:"data,omitempty"`
 }
 
 func SuccessResponse[T any](data T) Response[T] {
 	return Response[T]{
 		Success: true,
 		Message: "Success",
-		Data:    data,
+		Data:    &data,
+	}
+}
+
+func SuccessEmptyResponse[T any]() Response[T] {
+	return Response[T]{
+		Success: true,
+		Message: "Success",
+		Data:    nil,
 	}
 }
 
