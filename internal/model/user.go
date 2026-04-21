502c507d7cd7f89942de8640977b72de96084217
sungjun-ever <zkfmak10@gmail.com>
2026-04-21 21:36:53 +0900

    model/user.go password 필드 응답 시 hidden처리

--- internal/model/user.go
+++ internal/model/user.go
@@ -8,7 +8,7 @@ type User struct {
 	gorm.Model
 	UUID     string               `gorm:"unique, not null" json:"uuid"`
 	Email    string               `gorm:"unique, not null" json:"email" binding:"required,email"`
-	Password string               `gorm:"not null" json:"password" binding:"required,min=6,max=20"`
+	Password string               `gorm:"not null" json:"-" binding:"required,min=6,max=20"`
 	Status   int                  `gorm:"default:0" json:"status"`
 	Profile  *Profile             `gorm:"foreignKey:UserID" json:"profile"`
 	Uba      *UserBelongApartment `gorm:"foreignKey:UserID" json: uba`
