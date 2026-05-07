6b85fd6b4a72f01d2b7da532d67ca3c29e0a89c3
sungjun-ever <zkfmak10@gmail.com>
2026-05-07 18:14:00 +0900

    구조 변경 누락 추가

---
+++ internals/apartment/domain/apartment.go
@@ -0,0 +1,15 @@
+package domain
+
+import "gorm.io/gorm"
+
+type Apartment struct {
+	gorm.Model
+	PublicID string  `gorm:"uniqueIndex;type:char(26);not null"`
+	KaptCode string  `gorm:"not null; unique" json:"kaptCode"`
+	KaptName string  `gorm:"not null" json:"kaptName"`
+	As1      *string `json:"as1"`
+	As2      *string `json:"as2"`
+	As3      *string `json:"as3"`
+	As4      *string `json:"as4"`
+	BjdCode  *string `json:"bjdCode"`
+}
