d2333314205d03e62ff8bf72976cf1ba443fd626
sungjun-ever <zkfmak10@gmail.com>
2026-05-07 14:07:14 +0900

    model/user.go AfterDelete hook 추가

--- internals/model/user.go
+++ internals/model/user.go
@@ -10,3 +10,15 @@ type User struct {
 	Profile  Profile `gorm:"foreignKey:UserID"`
 	Roles    []Role  `gorm:"many2many:user_belong_apartments"`
 }
+
+func (u *User) AfterDelete(tx *gorm.DB) error {
+	if err := tx.Model(&User{}).Where("id = ?", u.ID).Delete(&User{}).Error; err != nil {
+		return err
+	}
+
+	if err := tx.Model(&UserBelongApartment{}).Where("user_id = ?", u.ID).Delete(&UserBelongApartment{}).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
