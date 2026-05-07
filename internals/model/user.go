33066d8fd504b608871d9ca2171e59606eb5e8dd
sungjun-ever <zkfmak10@gmail.com>
2026-05-07 13:53:22 +0900

    model/user.go: Profile 외래키 추가

--- internals/model/user.go
+++ internals/model/user.go
@@ -4,9 +4,9 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	PublicID string `gorm:"uniqueIndex;type:char(26);not null"`
-	Email    string `gorm:"unqiueIndex; not null" json:"email"`
-	Password string `josn:"password; not null"`
-	Profile  Profile
-	Roles    []Role `gorm:"many2many:user_belong_apartments"`
+	PublicID string  `gorm:"uniqueIndex;type:char(26);not null"`
+	Email    string  `gorm:"unqiueIndex; not null" json:"email"`
+	Password string  `josn:"password; not null"`
+	Profile  Profile `gorm:"foreignKey:UserID"`
+	Roles    []Role  `gorm:"many2many:user_belong_apartments"`
 }
