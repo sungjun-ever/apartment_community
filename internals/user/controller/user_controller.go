484a821bb85c9004d7b6216ffd5fd5d67bf918c3
sungjun-ever <zkfmak10@gmail.com>
2026-05-08 14:58:42 +0900

    사용자 생성 및 조회 로직 구현

--- internals/user/controller/user_controller.go
+++ internals/user/controller/user_controller.go
@@ -17,6 +17,46 @@ func NewUserController(us service.UserService) *UserController {
 	return &UserController{us: &us}
 }
 
+func (uc *UserController) GetUsers(c *gin.Context) {
+	var pr domain.PaginationRequest
+
+	if err := c.ShouldBindQuery(&pr); err != nil {
+		_ = c.Error(errUtils.NewAppError(err, 400, "C001"))
+		return
+	}
+
+	users, total, err := uc.us.FindUsers(c, pr)
+
+	if err != nil {
+		_ = c.Error(err)
+		return
+	}
+
+	response.OK(c, 200, domain.NewUserResources(users), &response.Meta{
+		Page:  pr.Page,
+		Size:  pr.Size,
+		Total: total,
+	})
+}
+
+func (uc *UserController) GetUser(c *gin.Context) {
+	var pidRequest domain.PublicIdUriRequest
+
+	if err := c.ShouldBindUri(&pidRequest); err != nil {
+		_ = c.Error(errUtils.NewAppError(err, 400, "C001"))
+		return
+	}
+
+	user, err := uc.us.FindUser(c, pidRequest)
+
+	if err != nil {
+		_ = c.Error(err)
+		return
+	}
+
+	response.OK(c, 200, domain.NewUserResource(user), nil)
+}
+
 func (uc *UserController) StoreUser(c *gin.Context) {
 	var rq domain.RegisterRequest
 
@@ -28,9 +68,9 @@ func (uc *UserController) StoreUser(c *gin.Context) {
 	user, err := uc.us.CreateUser(c, rq)
 
 	if err != nil {
-		_ = c.Error(errUtils.NewAppError(err, 500, "S001"))
+		_ = c.Error(err)
 		return
 	}
 
-	response.OK(c, 201, user, nil)
+	response.OK(c, 201, domain.NewUserResource(user), nil)
 }
--- internals/user/domain/user_request.go
+++ internals/user/domain/user_request.go
@@ -1,18 +1,29 @@
 package domain
 
-type ProfileRequest struct {
-	NickName string `json:"name"`
-	ImageID  *uint  `json:"imageId" binding:"omitempty"`
+type PublicIdUriRequest struct {
+	PublicID string `uri:"publicID"`
 }
-type UserRequest struct {
-	Email           string `json:"email" binding:"required,email"`
-	Password        string `json:"password" binding:"required,min=6,max=16"`
-	PasswordConfirm string `json:"passwordConfirm" binding:"required,eqfield=Password"`
+
+type PaginationRequest struct {
+	Page   int  `form:"page,default=1" binding:"min=1"`
+	Size   int  `form:"size,default=10" binding:"min=1,max=100"`
+	IsDesc bool `form:"isDesc;default=true"`
+}
+
+type UserAuthBase struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6,max=16"`
+}
+
+type ProfileBase struct {
+	NickName string `json:"nickname" binding:"omitempty,min=2,max=16"`
+	ImageID  *uint  `json:"imageId" binding:"omitempty"`
 }
 
 type RegisterRequest struct {
-	UserRequest
-	ProfileRequest
+	UserAuthBase
+	ProfileBase
+	PasswordConfirm string `json:"passwordConfirm" binding:"required,eqfield=Password"`
 }
 
 func (r *RegisterRequest) ToUserEntity() *User {
