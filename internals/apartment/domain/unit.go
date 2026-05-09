1d8a7e800610ca442a659aafb677eb69891aebe0
sungjun-ever <zkfmak10@gmail.com>
2026-05-09 15:39:12 +0900

    domain/unit.go fk 추가

--- internals/apartment/domain/unit.go
+++ internals/apartment/domain/unit.go
@@ -1,9 +1,14 @@
 package domain
 
-import "gorm.io/gorm"
+import (
+	userDomain "apart_community/internals/user/domain"
+
+	"gorm.io/gorm"
+)
 
 type Unit struct {
 	gorm.Model
-	BuildingID uint   `gorm:"not null;index" json:"buildingId"`
-	No         string `gorm:"not null" json:"no"`
+	BuildingID uint                      `gorm:"not null;index" json:"buildingId"`
+	No         string                    `gorm:"not null" json:"no"`
+	UnitsRoles []userDomain.UserUnitRole `gorm:"foreignKey:UnitID"`
 }
