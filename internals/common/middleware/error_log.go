afa0164c1fa141c62b7408b7587424c5af563b01
sungjun-ever <zkfmak10@gmail.com>
2026-05-16 15:36:50 +0900

    middleware/error_log.go 수정 1. ErrorManager 주입 받도록 수정 2. 기존 slog 처리를 ErrorManager에서 처리하고 에러 응답만 반환하도록 변경

--- internals/common/middleware/error_log.go
+++ internals/common/middleware/error_log.go
@@ -4,34 +4,24 @@ import (
 	"apart_community/internals/common/errUtils"
 	"apart_community/internals/common/response"
 	"errors"
-	"log/slog"
 
 	"github.com/gin-gonic/gin"
 )
 
-func ErrorLogMiddleWare() gin.HandlerFunc {
+func ErrorLogMiddleWare(em *errUtils.ErrorManager) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Next()
 
 		if len(c.Errors) > 0 {
 			err := c.Errors.Last().Err
 			var appErr *errUtils.AppError
-			traceID, _ := c.Get("trace_id")
+
+			em.Handle(c, err)
 
 			if errors.As(err, &appErr) {
-				slog.Error("Application Error",
-					"trace_id", traceID,
-					"code", appErr.Status,
-					"message", appErr.Message,
-					"error", appErr.Err,
-				)
-				response.Fail(c, appErr.Status, appErr.Code, appErr.Message)
+				response.Fail(c, appErr.Status, appErr.Code.String(), appErr.Code.GetMessage())
 			} else {
-				slog.Error("Unknown Error",
-					"trace_id", traceID,
-					"error", err,
-				)
-				response.Fail(c, 500, "S001", "INTERNAL_SERVER_ERROR")
+				response.Fail(c, 500, errUtils.S001.String(), "INTERNAL_SERVER_ERROR")
 			}
 		}
 	}
